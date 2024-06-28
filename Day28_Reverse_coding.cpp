11b3be27a44544442928c9a7e9b1b18856a3eba6
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-06-28 23:42:46 +0530

    Day 28

---
+++ Day28_Reverse_coding.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int sumOfNaturals(int n) {
+        // code here
+        long MOD=1000000007;
+        long a = n;
+        long b = (n+1);
+        return ((a*b)/2)%(MOD);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        
+        cin>>n;
+
+        Solution ob;
+        cout << ob.sumOfNaturals(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
---
+++ Day_28_Trailing_zeroes_in_factorial.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    int trailingZeroes(int N)
+    {
+        int n=N;
+        int res=0;
+        while(n>0){
+            res +=(n/5);
+            n/=5;
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        int ans  = ob.trailingZeroes(N);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
