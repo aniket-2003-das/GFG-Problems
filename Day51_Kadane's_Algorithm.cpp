4e1f42e134ce6fee373b80fd4e74b45093b9c774
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-07-21 11:07:27 +0530

    Day 51

---
+++ Day51_Kadane's_Algorithm.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    // Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(vector<int> &arr) {
+        // code here...
+        long long maxi = LONG_MIN;
+        long long sum=0;
+        
+        for(int i=0; i<arr.size(); i++){
+            sum+=arr[i];
+            if(sum>maxi) maxi=sum;
+            if(sum<0) sum=0;
+        }
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To discard any leftover newline characters
+    while (t--)   // while testcases exist
+    {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.maxSubarraySum(arr) << endl;
+    }
+}
+// } Driver Code Ends
