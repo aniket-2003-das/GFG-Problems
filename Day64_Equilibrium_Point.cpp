229f7bd20f742e40810175e465c21dd9c5e9c33a
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-08-04 22:26:29 +0530

    Day 64

---
+++ Day64_Equilibrium_Point.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find equilibrium point in the array.
+    // arr: input array
+    int equilibriumPoint(vector<long long> &arr) {
+        // Your code here
+        int n=arr.size();
+        int sum=0;
+        
+        for(int i=0; i<n; i++) sum+=arr[i];
+        
+        int lsum = 0;
+        for(int i=0; i<n; i++){
+            lsum+=arr[i];
+            if(sum==lsum) return i+1;
+            sum-=arr[i];
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To discard any leftover newline characters
+    while (t--)   // while testcases exist
+    {
+        vector<long long> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.equilibriumPoint(arr) << endl;
+    }
+}
+// } Driver Code Ends
