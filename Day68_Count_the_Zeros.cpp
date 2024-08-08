72b1127b89bdcea951bdaa5e6e74bd59859a7ff2
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-08-09 00:03:30 +0530

    Day 69

--- Day68_Count_the_Zeros.cpp
+++ Day68_Count_the_Zeros.cpp
@@ -1,3 +1,11 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
 // User function template for C++
 class Solution {
 public:
@@ -8,3 +16,27 @@ public:
         return count;
     }
 };
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.countZeroes(arr) << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
