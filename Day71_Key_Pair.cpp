6c928022d74311491491bd6242ea88fceda2ca1a
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-08-11 22:42:48 +0530

    Day 71

---
+++ Day71_Key_Pair.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    // Function to check if array has 2 elements
+    // whose sum is equal to the given value
+    bool hasArrayTwoCandidates(vector<int>& arr, int x) {
+        // code here
+        sort(arr.begin(), arr.end());
+        int left=0;
+        int right=arr.size()-1;
+        
+        // Use two-pointer approach to find the pair
+        while (left<right) {
+            int sum=arr[left]+arr[right];
+            if (sum==x) return true;
+            else if (sum<x) left++;
+            else right--;
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To discard any leftover newline characters
+    while (t--) {
+        int x;
+        cin >> x;
+        cin.ignore(); // To discard any leftover newline characters
+
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        auto ans = ob.hasArrayTwoCandidates(arr, x);
+        cout << (ans ? "true" : "false") << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
