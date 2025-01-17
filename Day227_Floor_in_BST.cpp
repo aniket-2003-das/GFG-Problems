f2e3d36cadbaf1ba1c66f82088315e8349803112
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2025-01-17 16:44:56 +0530

    Day 227

---
+++ Day227_Floor_in_BST.cpp
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node *right;
+    Node *left;
+
+    Node(int x) {
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+// Function to search a node in BST.
+class Solution{
+public:
+    int floor(Node* root, int x) {
+        // Code here
+        int flor = -1;
+        while(root){
+            if(root->data==x){
+                flor = root->data;
+                return flor;
+            }
+            if(x>root->data){
+                flor = root->data;
+                root = root->right;
+            }
+            else root = root->left;
+        }
+        return flor;
+    }
+};
+
+//{ Driver Code Starts.
+
+Node *insert(Node *tree, int val) {
+    Node *temp = NULL;
+    if (tree == NULL) return new Node(val);
+
+    if (val < tree->data) {
+        tree->left = insert(tree->left, val);
+    } else if (val > tree->data) {
+        tree->right = insert(tree->right, val);
+    }
+
+    return tree;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        Node *root = NULL;
+
+        int N;
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            int k;
+            cin >> k;
+            root = insert(root, k);
+        }
+
+        int s;
+        cin >> s;
+        Solution obj;
+        cout << obj.floor(root, s) << "\n";
+    
+cout << "~" << "\n";
+}
+}
+
+// } Driver Code Ends
