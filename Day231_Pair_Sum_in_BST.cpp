cf1514e9df16cc44e339fd6c2d5d811ecbde42b8
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2025-01-21 00:24:21 +0530

    Day 231

---
+++ Day231_Pair_Sum_in_BST.cpp
@@ -0,0 +1,174 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+#define MAX_SIZE 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+/*Complete the function below
+Node is as follows
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class BSTIterator {
+private: stack<Node*> mystack; 
+// reverse->True->before
+// reverse->False->next
+bool reverse = true;
+public:
+    BSTIterator(Node* root, bool isReverse) {
+        reverse = isReverse;
+        pushAll(root);
+    }
+    
+    bool hasNext() {
+        return !mystack.empty();
+    }
+
+    int next() {
+        Node* tempNode = mystack.top();
+        mystack.pop();
+        if(!reverse) pushAll(tempNode->right);
+        else pushAll(tempNode->left);
+        return tempNode->data;
+    }
+private:
+    void pushAll(Node* node){
+        for(; node!=NULL;){
+            mystack.push(node);
+            if(reverse==true) node=node->right;
+            else node=node->left;
+        }
+    }
+};
+class Solution {
+  public:
+    // root : the root Node of the given BST
+    // target : the target sum
+    
+    int findTarget(struct Node* root, int k) {
+        if(!root) return false;
+        BSTIterator l(root, false);
+        BSTIterator r(root, true);
+
+        int i = l.next();
+        int j = r.next();
+        while(i<j){
+            if(i+j==k) return true;
+            else if(i+j<k) i=l.next();
+            else j = r.next();
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
+
+        getline(cin, s);
+        int k = stoi(s);
+        // getline(cin, s);
+        Solution obj;
+        cout << obj.findTarget(root, k) << endl;
+        // cout<<"~"<<endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
