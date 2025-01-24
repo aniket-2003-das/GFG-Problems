f71f9daa4c1048c066d4278f0efd7beaf122ccae
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2025-01-24 17:42:17 +0530

    Day 234

---
+++ BFS of graph.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(vector<vector<int>> &adj) {
+        // Code here
+        unordered_map<int,bool> visited;
+        visited[0]=true;
+        
+        vector<int> ans;
+        queue<int> q;
+        q.push(0);
+    
+        while(!q.empty()){
+            int Node = q.front();
+            q.pop();
+            ans.push_back(Node);
+            for(auto i: adj[Node]){
+                if(!visited[i]){
+                    q.push(i);
+                    visited[i] = true;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+
+        // Now using vector of vectors instead of array of vectors
+        vector<vector<int>> adj(V);
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u); // For undirected graph, add both u->v and v->u
+        }
+
+        Solution obj;
+        vector<int> ans = obj.bfsOfGraph(adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
